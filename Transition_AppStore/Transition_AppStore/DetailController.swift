d17229f16435a466ff76605f26a58aa1819b56dc
Memo <ginppian@gmail.com>
2020-03-05 16:17:17 -0600

    initial

---
+++ Transition_AppStore/Transition_AppStore/DetailController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailController.swift
+//  Transition_AppStore
+//
+//  Created by Memo on 05/03/20.
+//  Copyright © 2020 Memo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Transition_AppStore/Transition_AppStore/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Transition_AppStore
+//
+//  Created by Memo on 05/03/20.
+//  Copyright © 2020 Memo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "UICollectionViewCell")
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UICollectionViewCell", for: indexPath) as UICollectionViewCell
+        cell.backgroundColor = .green
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: collectionView.bounds.height/2)
+    }
+}
